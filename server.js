f3f0ed86ec34e6996425bae7ea42212d5d4c0f5e
rivoalpha <rrajaonarivony.ext@baobabgroup.com>
2024-02-10 22:46:50 +0300

    [ADD] Login user

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const db = require('./utils/db');
-const app = express();
+
 
 db.connect();
 
+const app = express();
 app.use(express.json());
 
 require('./routes/user')(app);
