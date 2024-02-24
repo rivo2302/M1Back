24c799c235d5a8620f3eef4a553d25ddbb2d3974
rivoalpha <rrajaonarivony.ext@baobabgroup.com>
2024-02-24 10:21:43 +0300

    [FIX] Cors headers

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control');
     next();
 });
 
