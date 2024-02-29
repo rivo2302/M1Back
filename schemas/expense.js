3a6a53a08a6a2a28230360100abd7899a16cba2e
rivoalpha <rrajaonarivony.ext@baobabgroup.com>
2024-02-29 07:50:31 +0300

    [ADD] Custom port

--- schemas/expense.js
+++ schemas/expense.js
@@ -5,6 +5,7 @@ const expenseSchema = new Schema({
   description: String,
   price: {type : Number , required : true },
   name:  {type : String , required : true },
+  date: {type : Date , required : true}
 });
 
 module.exports = expenseSchema;
